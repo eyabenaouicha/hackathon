850b00e5054c4807f643163ed590e523c3edbb41
Sameh manai <Samehmanai8@gmail.com>
2024-12-22 12:47:59 +0100

    hhhhh

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
         <div className="hero py-12 bg-gradient-to-t from-blue-500 to-purple-700">
             <div className="hero-content md:px-0 px-4 max-w-6xl flex-col lg:flex-row-reverse">
                 <img 
-                    src="https://plus.unsplash.com/premium_photo-1681319553238-9860299dfb0f?auto=format&fit=crop&q=80&w=2831&ixlib=rb-4.0.3" 
+                    src="job.png" 
                     className="max-w-sm h-80 object-cover rounded-lg shadow-2xl" 
                     alt="Hexabot illustration" 
                 />
